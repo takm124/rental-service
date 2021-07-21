206a4642cd3642ea7fa192a569dcc263b9dcebef
noah <ssh7636@gmail.com>
2021-07-21 14:13:25 +0900

    초기모델

---
+++ gamsung/src/test/java/com/gamsung/GamsungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamsung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamsungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
